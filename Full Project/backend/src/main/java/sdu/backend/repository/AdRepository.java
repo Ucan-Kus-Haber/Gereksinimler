05aa96fd7f6d7108d63c9580a3cf600a870859f5
Azat Vepakulyyev <126404974+Arious18@users.noreply.github.com>
2025-06-21 15:23:29 +0300

    Delete Full Project directory

--- Full Project/backend/src/main/java/sdu/backend/repository/AdRepository.java
+++
@@ -1,8 +0,0 @@
-package sdu.backend.repository;
-
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import sdu.backend.model.Ad;
-
-public interface AdRepository extends MongoRepository<Ad, String> {
-}
